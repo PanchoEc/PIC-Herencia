ece5eb069402d226cd6265efd10b462a21ebd788
Patricio <Patricio@LAPTOP-EGK96D85>
2024-01-21 18:02:47 -0500

    Add project files.

---
+++ PIC-master/PIC/VectorInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    public class VectorInput : Vector
+    {
+        
+        public VectorInput(double x, double y, double z) : base(x, y, z)
+        {
+        }
+
+        public static VectorInput Usuario(string prompt)
+        {
+            
+            double x = InputValidator.GetDoubleInput(prompt + " Componente X: ");
+            double y = InputValidator.GetDoubleInput(prompt + " Componente Y: ");
+            double z = InputValidator.GetDoubleInput(prompt + " Componente Z: ");
+            VectorInput vector = new VectorInput(x,y,z);
+            return vector;
+        }
+
+     }
+}
+
+   
---
+++ PIC-master/PIC/VectorOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    public class VectorOperations:Vector
+    {
+        public VectorOperations(double x, double y, double z) : base(x, y, z)
+        {
+        }
+
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            VectorOperations add = new VectorOperations(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+            return add;
+        }
+
+        
+        public static Vector Subtract(Vector v1, Vector v2)
+        {
+            VectorOperations subs = new VectorOperations(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+            return subs; 
+         }
+
+        public static double Product(Vector v1, Vector v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+        }
+    }
+}
